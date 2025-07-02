6e1ed9f12099ea36ea88d0de4a7c1a4b446d426a
Badriddin <s321451@studenti.polito.it>
2025-07-02 10:18:55 +0500

    New Layout

---
+++ app/src/main/java/com/example/spendingtrackerlite/AddProductFragment.java
@@ -0,0 +1,27 @@
+package com.example.spendingtrackerlite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class AddProductFragment extends Fragment {
+    public AddProductFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_product, container, false);
+    }
+}
--- app/src/main/java/com/example/spendingtrackerlite/MainActivity.java
+++ app/src/main/java/com/example/spendingtrackerlite/MainActivity.java
@@ -1,74 +1,80 @@
 package com.example.spendingtrackerlite;
 
-import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
+
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.example.spendingtrackerlite.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editTextCategory, editTextType, editTextBrand, editTextTitle,
-            editTextUnit, editTextQuantity, editTextPercent, editTextBarcode,
-            editTextManufacturer, editTextCountry;
-    Button buttonInsert, buttonViewProducts;
-    DatabaseHelper dbHelper;
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        editTextCategory = findViewById(R.id.editTextCategory);
-        editTextType = findViewById(R.id.editTextType);
-        editTextBrand = findViewById(R.id.editTextBrand);
-        editTextTitle = findViewById(R.id.editTextTitle);
-        editTextUnit = findViewById(R.id.editTextUnit);
-        editTextQuantity = findViewById(R.id.editTextQuantity);
-        editTextPercent = findViewById(R.id.editTextPercent);
-        editTextBarcode = findViewById(R.id.editTextBarcode);
-        editTextManufacturer = findViewById(R.id.editTextManufacturer);
-        editTextCountry = findViewById(R.id.editTextCountry);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
-        buttonInsert = findViewById(R.id.buttonInsert);
-        buttonViewProducts = findViewById(R.id.buttonViewProducts);
+        setSupportActionBar(toolbar);
 
-        dbHelper = new DatabaseHelper(this);
-        dbHelper.createDatabase();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
 
-        buttonInsert.setOnClickListener(view -> {
-            String category = editTextCategory.getText().toString().trim();
-            String type = editTextType.getText().toString().trim();
-            String brand = editTextBrand.getText().toString().trim();
-            String title = editTextTitle.getText().toString().trim();
-            String unit = editTextUnit.getText().toString().trim();
-            String quantityInput = editTextQuantity.getText().toString().trim();
-            Double quantity = null;
-            if (!quantityInput.isEmpty()) quantity = Double.parseDouble(quantityInput);
-            String percentInput = editTextPercent.getText().toString().trim();
-            Double percent = null;
-            if (!percentInput.isEmpty()) percent = Double.parseDouble(percentInput);
-            String barcode = editTextBarcode.getText().toString().trim();
-            String manufacturer = editTextManufacturer.getText().toString().trim();
-            String country = editTextCountry.getText().toString().trim();
-            dbHelper.insertProduct(category, type, brand, title, unit, quantity, percent, barcode, manufacturer, country);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-        });
-
-        buttonViewProducts.setOnClickListener(new View.OnClickListener() {
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ProductListActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+
+                if (menuItem.getItemId() == R.id.nav_home) selectedFragment = new HomeFragment();
+                else if (menuItem.getItemId() == R.id.nav_add) selectedFragment = new AddProductFragment();
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main_content, selectedFragment)
+                            .commit();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
+
+        // Show default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_content, new HomeFragment())
+                    .commit();
+            navView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
